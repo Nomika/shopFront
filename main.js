cd7fafb76e1db35e265d253a694a3e18c5ebf569
Nomika <1622@wp.pl>
2022-05-17 20:34:03 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+getAllData = () => {
+
+    let resultElement = document.getElementById('getResult');
+  
+    resultElement.innerHTML = '';
+  
+    fetch('http://localhost:3000/products')
+  
+      .then((result) => {
+  
+        if (!result.ok) {
+  
+          throw new Error(`Błąd: ${result.status} - ${result.statusText}`);
+  
+        }
+  
+        return result.json();
+  
+      })
+  
+      .then((data) => {
+  
+        resultElement.innerHTML =
+  
+          "<div class='alert alert-secondary mt-2' role='alert'><pre>" +
+  
+          JSON.stringify(data, null, 2) +
+  
+          '</pre></div>';
+  
+      });
+  
+  };
